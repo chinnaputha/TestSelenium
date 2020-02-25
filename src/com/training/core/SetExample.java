b723ae071a0707d69491ea837cfedb534372ee59
Chinna.Putha <chinna.putha@gmail.com>
2020-02-25 15:35:19 +0530

    New Project

---
+++ src/com/training/core/SetExample.java
@@ -0,0 +1,25 @@
+package com.training.core;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetExample {
+
+	public static void main(String[] args) {
+		Set<String> al = new HashSet<String>();
+		al.add("java");
+		al.add("selenium");
+		al.add("TestNG");
+		al.add("Apache POI");
+		al.add("java");
+		
+		
+		System.out.println("set of values-->"+al.size());
+		System.out.println("set of values-->"+al);
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/com/training/core/TestInt.java
@@ -0,0 +1,11 @@
+package com.training.core;
+
+public interface TestInt {
+	
+	void add(); // public abstract void add();
+	
+	void sub();
+
+	
+	
+}
